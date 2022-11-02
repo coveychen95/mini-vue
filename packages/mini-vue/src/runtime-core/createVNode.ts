6e717aaa234b8849443acdc2b2659023706d85b6
coveychen95 <coveychen95@outlook.com>
2022-11-02 21:14:08 +0800

    feat: createVNode children

--- packages/mini-vue/src/runtime-core/createVNode.ts
+++ packages/mini-vue/src/runtime-core/createVNode.ts
@@ -13,6 +13,14 @@ export function createVNode(
     children,
     shapeFlag: getShapeFlag(type)
   }
+
+  // 基于 children 再次设置 shapeFlag
+  if (Array.isArray(children)) {
+    vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN
+  } else if (typeof children === 'string'){
+    vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN
+  }
+  
   return vnode
 }
 
